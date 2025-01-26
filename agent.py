458240b07c035b3c089a11e7a43810e0ec1c8e59
luanvfm <luanventurafm2@gmail.com>
2025-01-26 20:27:55 -0300

    Teste para Seletiva

--- agent.py
+++ agent.py
@@ -1,19 +1,38 @@
 from utils.ssl.Navigation import Navigation
 from utils.ssl.base_agent import BaseAgent
-
+from utils.Geometry import Geometry
 class ExampleAgent(BaseAgent):
+    
     def __init__(self, id=0, yellow=False):
         super().__init__(id, yellow)
 
     def decision(self):
+        position = self.pos
+        obstacles = [self.opponents]
+        target = self.targets
+
+        def is_collision_and_act(position, target, obstacles, clearance=100):
+            for obstacle in obstacles:  
+                distance = Geometry.dist_to(position, (obstacle["x"], obstacle["y"]))
+                if distance < clearance:                
+                    target_velocity, target_angle_velocity = Navigation.goToPoint(
+                    self.robot, (obstacle["x"] + 50, obstacle["y"] + 40)
+                )
+                    self.set_vel(target_velocity)
+                    self.set_angle_vel(target_angle_velocity)
+                    return True
+                else:
+                    return False
+
         if len(self.targets) == 0:
             return
-
-        target_velocity, target_angle_velocity = Navigation.goToPoint(self.robot, self.targets[0])
-        self.set_vel(target_velocity)
-        self.set_angle_vel(target_angle_velocity)
-
-        return
+        if is_collision_and_act(position, target, obstacles):
+            pass
+        else:            
+            target_velocity, target_angle_velocity = Navigation.goToPoint(self.robot, self.targets[0])
+            self.set_vel(target_velocity)
+            self.set_angle_vel(target_angle_velocity) 
+             
 
     def post_decision(self):
         pass
